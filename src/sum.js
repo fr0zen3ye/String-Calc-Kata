5dc9dca179e30d3e8625127d7ce266e652ce6509
Stefan Golker <stefangolker@Stefans-MBP.fritz.box>
2021-02-09 20:51:34 +0100

    correct tests

--- src/sum.js
+++ src/sum.js
@@ -2,14 +2,14 @@ function stringCalc(numbers) {
 
     let input = String(numbers);
     let stringSplit = input.split(/[\n,+;\_\/]/).filter(it => it);
-    console
     if(stringSplit.length > 1){
 
         let addition = 0
         for (i = 0; i<stringSplit.length ;i++) {
             addition = addition + parseInt(stringSplit[i]);
             if (stringSplit[i] < 0) {
-                console.log("negatives not allowed :")
+
+                console.log("negatives not allowed: " + numbers)
                 throw new Error("negatives not allowed");
             }
         }
